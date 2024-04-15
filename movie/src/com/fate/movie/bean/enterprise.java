ae90059259815c4119c9831a81ddff4772226e36
SpShi <spshi81@gmail.com>
2024-04-15 19:37:52 +0800

    add java class "administrator" and "enterprise" in dir "bean"

---
+++ movie/src/com/fate/movie/bean/enterprise.java
@@ -0,0 +1,73 @@
+package com.fate.movie.bean;
+
+public class enterprise {
+    private long id;
+    private long license;
+    private String idnumber;
+    private String name;
+    private String ename;
+    private long tel;
+    private int state;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public long getlicense() {
+        return license;
+    }
+
+    public void setlicense(long license) {
+        this.license = license;
+    }
+
+    public String getidnumber() {
+        return idnumber;
+    }
+
+    public void setidnumber(String idnumber) {
+        this.idnumber = idnumber;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public long gettel() {
+        return tel;
+    }
+
+    public void settel(long tel) {
+        this.tel = tel;
+    }
+
+    public int getstate(){
+        return state;
+    }
+
+    public void setstate(int state){
+        this.state=state;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", license='" + license + '\'' +
+                ", idnumber='" + idnumber + '\'' +
+                ", name='" + name + '\'' +
+                ", ename='" + ename + '\'' +
+                ", tel='" + tel + '\'' +
+                ", state='" + state + '\'' +
+                '}';
+    }
+}
